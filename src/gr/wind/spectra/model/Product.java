d6021941b62d3e1f773788373573cb0846340f86
Apostolos <ap.kapetanios@gmail.com>
2019-07-07 00:58:42 +0300

    First Draft of getHierarchy

---
+++ src/gr/wind/spectra/model/Product.java
@@ -0,0 +1,45 @@
+package gr.wind.spectra.model;
+
+import java.util.List;
+
+import javax.xml.bind.annotation.*;
+
+@XmlRootElement(name = "Element")
+@XmlType(propOrder = {"type", "item"})
+public class Product {
+	
+	private String type;
+	private List<String> item;
+	
+	// Empty constructor requirement of JAXB (Java Architecture for XML Binding)
+	public Product()
+	{
+	}
+	
+	public Product(String type, List<String> valuesList)
+	{
+		this.type = type;
+		this.item = valuesList;
+	}
+	
+	@XmlElement(name = "elementType")
+	public String getType()
+	{
+		return type;
+	}
+	
+	public void setType(String type)
+	{
+		this.type = type;
+	}
+	
+	public List<String> getitem()
+	{
+		return item;
+	}
+	
+	public void setitem(List<String> valuesList)
+	{
+		this.item = valuesList;
+	}
+}
--- src/gr/wind/spectra/web/WebSpectra.java
+++ src/gr/wind/spectra/web/WebSpectra.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
+import javax.jws.WebResult;
 import javax.jws.WebService;
 
 import gr.wind.spectra.web.InvalidInputException;
@@ -14,7 +15,7 @@ import gr.wind.spectra.web.InvalidInputException;
 import gr.wind.spectra.business.DB_Connection;
 import gr.wind.spectra.business.DB_Operations;
 import gr.wind.spectra.business.Help_Func;
-import gr.wind.spectra.business.ServiceImplementation;
+import gr.wind.spectra.model.Product;
 
 @WebService //(endpointInterface = "gr.wind.spectra.web.WebSpectraInterface")
 public class WebSpectra// implements WebSpectraInterface
@@ -29,22 +30,10 @@ public class WebSpectra// implements WebSpectraInterface
 		conn = conObj.Connect();
 		dbs = new DB_Operations(conn);
 	}
-
-	@WebMethod
-	public List<String> getRootElements()
-	{
-		List<String> rootElementsList = new ArrayList<String>();
-    	try {
-			rootElementsList = dbs.GetOneColumnResultSet("rootNetworkElementHierarchy", "ElementType", "1 = 1");
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return rootElementsList;
-	}
 	
 	@WebMethod
-	public List<String> getHierarchy(
+	@WebResult(name="Result")
+	public List<Product> getHierarchy(
 			@WebParam(name="Type") String Type,
 			@WebParam(name="Level1") String Level1,
 			@WebParam(name="Level2") String Level2,
@@ -59,100 +48,62 @@ public class WebSpectra// implements WebSpectraInterface
 			) throws gr.wind.spectra.web.InvalidInputException
 	{
 		List<String> rootElementsList = new ArrayList<String>();
-		
+		List<Product> prodElementsList = new ArrayList<>();
 		// FTTX Hierarchy
 		if (Type == null || Type.equals("") || Type.equals("?"))
 		{
-			rootElementsList = dbs.GetOneColumnUniqueResultSet("rootNetworkElementHierarchy", "ElementType", "1 = 1");
+				rootElementsList = dbs.GetOneColumnUniqueResultSet("rootNetworkElementHierarchy", "ElementType", "1 = 1");
+				Product pr = new Product("rootElements", rootElementsList);
+				prodElementsList.add(pr);
 		}
 		else if (Type.equals("FTTX") && ( Level1 == null || Level1.equals("") || Level1.equals("?") ) )
 		{
 			rootElementsList = dbs.GetOneColumnUniqueResultSet("FTTX_NetworkElementHierarchy", "OltElementName", "1 = 1");
+			Product pr = new Product("OltElementName", rootElementsList);
+			prodElementsList.add(pr);
 		}
 		else if (Type.equals("FTTX") && ( Level2 == null || Level2.equals("") || Level2.equals("?") ) )
 		{
-			String predicates = Help_Func.AssignSimilarANDPredicates(new String[] {"OltElementName"}, new String[] { Level1 });
 			rootElementsList = dbs.GetOneColumnUniqueResultSet("FTTX_NetworkElementHierarchy", "GponSlotNo", "1 = 1");
+			Product pr = new Product("GponSlotNo", rootElementsList);
+			prodElementsList.add(pr);
 		}
 		else if (Type.equals("FTTX") && ( Level3 == null || Level3.equals("") || Level3.equals("?") ))
 		{
 			String predicates = Help_Func.AssignSimilarANDPredicates(new String[] {"OltElementName", "GponSlotNo"}, new String[] { Level1, Level2 });
-			rootElementsList =  dbs.GetOneColumnUniqueResultSet("FTTX_NetworkElementHierarchy", "PonNo", predicates);	
+			rootElementsList =  dbs.GetOneColumnUniqueResultSet("FTTX_NetworkElementHierarchy", "PonNo", predicates);
+			Product pr = new Product("PonNo", rootElementsList);
+			prodElementsList.add(pr);
 		}
 		else if (Type.equals("FTTX") && ( Level4 == null || Level4.equals("") || Level4.equals("?") ))
-		{
+		{				
 			String predicates = Help_Func.AssignSimilarANDPredicates(new String[] {"OltElementName", "GponSlotNo", "PonNo"}, new String[] { Level1, Level2, Level3 });
 			rootElementsList =  dbs.GetOneColumnUniqueResultSet("FTTX_NetworkElementHierarchy", "OnuPortNo", predicates);
+			Product pr = new Product("OnuPortNo", rootElementsList);
+			prodElementsList.add(pr);
 		}
 		else if (Type.equals("FTTX") && ( Level5 == null || Level5.equals("") || Level5.equals("?") ))
 		{
 			String predicates = Help_Func.AssignSimilarANDPredicates(new String[] {"OltElementName", "GponSlotNo", "PonNo", "OnuPortNo"}, new String[] { Level1, Level2, Level3, Level4 });
 			rootElementsList =  dbs.GetOneColumnUniqueResultSet("FTTX_NetworkElementHierarchy", "NgaElementName", predicates);
+			Product pr = new Product("NgaElementName", rootElementsList);
+			prodElementsList.add(pr);
 		}
 		else if (Type.equals("FTTX") && ( Level6 == null || Level6.equals("") || Level6.equals("?") ))
 		{
 			String predicates = Help_Func.AssignSimilarANDPredicates(new String[] {"OltElementName", "GponSlotNo", "PonNo", "OnuPortNo", "NgaElementName"}, new String[] { Level1, Level2, Level3, Level4, Level5 });
 			rootElementsList =  dbs.GetOneColumnUniqueResultSet("FTTX_NetworkElementHierarchy", "NgaElementCardSlotNo", predicates);
+			Product pr = new Product("NgaElementCardSlotNo", rootElementsList);
+			prodElementsList.add(pr);
 		}
 		else if (Type.equals("FTTX") && ( Level6 != null || ! Level6.equals("") || ! Level6.equals("?") ))
 		{
 			throw new InvalidInputException("Invalid Input", "There is no Level 7 for FTTX type");
 		}
-		// FTTX Hierarchy
-		
-		
-		
-		
-		return rootElementsList;
+
+		return prodElementsList;
 	}
 	
-	
-	/*
-	public List<String> getFTTXHierarchy(String OltElementName, 
-			String GponSlotNo, 
-			String PonNo, 
-			String OnuPortNo, 
-			String NgaElementName)
-	{
-		List<String> rootElementsList = new ArrayList<String>();
-		
-		if (OltElementName == null || OltElementName.equals("") || OltElementName.equals("?"))
-		{
-			rootElementsList = dbs.GetOneColumnUniqueResultSet("FTTX_NetworkElementHierarchy", "OltElementName", "1 = 1");
-		}
-		else if (GponSlotNo == null || GponSlotNo.equals("") ||GponSlotNo.equals("?"))
-		{
-			String predicates = Help_Func.AssignSimilarANDPredicates(new String[] {"OltElementName"}, new String[] { OltElementName });
-			rootElementsList =  dbs.GetOneColumnUniqueResultSet("FTTX_NetworkElementHierarchy", "GponSlotNo", predicates);
-		}
-		else if (PonNo == null || PonNo.equals("") || PonNo.equals("?"))
-		{
-			String predicates = Help_Func.AssignSimilarANDPredicates(new String[] {"OltElementName", "GponSlotNo"}, new String[] { OltElementName, GponSlotNo });
-			rootElementsList =  dbs.GetOneColumnUniqueResultSet("FTTX_NetworkElementHierarchy", "PonNo", predicates);			
-		}
-		else if (OnuPortNo == null || OnuPortNo.equals("") || OnuPortNo.equals("?"))
-		{
-			String predicates = Help_Func.AssignSimilarANDPredicates(new String[] {"OltElementName", "GponSlotNo", "PonNo"}, new String[] { OltElementName, GponSlotNo, PonNo });
-			rootElementsList =  dbs.GetOneColumnUniqueResultSet("FTTX_NetworkElementHierarchy", "OnuPortNo", predicates);	
-		}
-		else if (NgaElementName == null || NgaElementName.equals("") || NgaElementName.equals("?"))
-		{
-			String predicates = Help_Func.AssignSimilarANDPredicates(new String[] {"OltElementName", "GponSlotNo", "PonNo", "OnuPortNo"}, new String[] { OltElementName, GponSlotNo, PonNo, OnuPortNo });
-			rootElementsList =  dbs.GetOneColumnUniqueResultSet("FTTX_NetworkElementHierarchy", "NgaElementName", predicates);			
-		}
-		else
-		{
-			String predicates = Help_Func.AssignSimilarANDPredicates(new String[] {"OltElementName", "GponSlotNo", "PonNo", "OnuPortNo", "NgaElementName"}, new String[] { OltElementName, GponSlotNo, PonNo, OnuPortNo, NgaElementName });
-			rootElementsList =  dbs.GetOneColumnUniqueResultSet("FTTX_NetworkElementHierarchy", "NgaElementCardSlotNo", predicates);	
-		}
-		return rootElementsList;
-	}
-	
-		
-	public int Add(int a, int b) 
-	{
-		return a+b;
-	}
 
 	public static void main(String args[]) throws SQLException
 	{/*
@@ -165,9 +116,8 @@ public class WebSpectra// implements WebSpectraInterface
 		{
 			System.out.println(item);
 		}
-		
+		*/
 	}
-	*/
 }
 
 
