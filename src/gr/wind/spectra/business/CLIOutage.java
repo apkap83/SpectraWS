b31c8e45382a9365310f6088a4232e55fa3503b4
Apostolos <ap.kapetanios@gmail.com>
2020-01-30 18:08:05 +0200

    Removing default Negative Responses from NLU_Active - LIVE in Production

--- src/gr/wind/spectra/business/CLIOutage.java
+++ src/gr/wind/spectra/business/CLIOutage.java
@@ -215,7 +215,7 @@ public class CLIOutage
 									new String[] { "String" }, new String[] { "IncidentID", "OutageID" },
 									new String[] { IncidentID, String.valueOf(OutageID) },
 									new String[] { "String", "Integer" });
-						
+
 							if (numOfRowsUpdated > 0)
 							{
 								logger.debug("ReqID: " + RequestID + " - Scheduled Incident: " + IncidentID
@@ -416,8 +416,8 @@ public class CLIOutage
 			}
 
 			// *************************************
-			// All NLU_Active responses are NEGATIVE 
-			foundAtLeastOneCLIAffected = false;
+			// All NLU_Active responses are NEGATIVE
+			// foundAtLeastOneCLIAffected = false;
 			// *************************************
 
 			// CLI is not affected from outage
