e5b77790838d5abad8e1dbd098fa5f0d8e83ca38
Apostolos <ap.kapetanios@gmail.com>
2021-03-06 20:40:36 +0200

    Seems Perfect!

--- src/gr/wind/spectra/consumerRequests/Async_GetHierarchy.java
+++ src/gr/wind/spectra/consumerRequests/Async_GetHierarchy.java
@@ -11,8 +11,8 @@ import javax.net.ssl.SSLSession;
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.X509TrustManager;
 
-import gr.wind.spectra.consumer.GetHierarchy;
-import gr.wind.spectra.consumer.WebSpectraService;
+import gr.wind.spectra.consumer.oneway.GetHierarchy;
+import gr.wind.spectra.consumer.oneway.WebSpectraService;
 
 public class Async_GetHierarchy extends Thread
 {
@@ -94,7 +94,7 @@ public class Async_GetHierarchy extends Thread
 
 		// Send request to Spectra_Reporting
 		WebSpectraService myWebService = new WebSpectraService();
-		gr.wind.spectra.consumer.InterfaceWebSpectra iws = myWebService.getWebSpectraPort();
+		gr.wind.spectra.consumer.oneway.InterfaceWebSpectra iws = myWebService.getWebSpectraPort();
 
 		GetHierarchy gh = new GetHierarchy();
 		gh.setRequestID(this.RequestID);
